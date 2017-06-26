34a7afdb0fa00364f5e840a7a281e4309ad8a5cc
AlexandruValeanu <valeanu_alex@yahoo.com>
2017-06-26 22:02:54 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import automata.dfa.DeterministicFiniteAutomaton;
+
 public class Main {
 
     public static void main(String[] args) {
+        DeterministicFiniteAutomaton dfa = DeterministicFiniteAutomaton.endInZero();
+        System.out.println(dfa.run("00000001110"));
     }
 }
